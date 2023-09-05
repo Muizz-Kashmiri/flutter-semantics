793ee1a1d426aa049bb0e6bdc7db5ec0423d123f
Muizz Elahi Butt <86423625+Muizz-Kashmiri@users.noreply.github.com>
2023-09-05 13:42:11 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/semantics_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.semantics_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Semantics Demo',
+      showSemanticsDebugger: true,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text("Flutter Semantics Demo"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Semantics(
+                label: 'Red square',
+                child: const SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(color: Colors.red),
+                  ),
+                ),
+              ),
+              Semantics(
+                label: 'Flutter logo',
+                child: const SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: FlutterLogo(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
